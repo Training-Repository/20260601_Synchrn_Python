2d36fa1a927318ed2587ddaec5f665a337a094ad
Ramakant Debata <ramakant.s.debata@gmail.com>
2026-06-02 13:32:02 +0530

    Day 02

--- 007_FunctionBasics.py
+++ 007_FunctionBasics.py
@@ -36,12 +36,94 @@
 #endregion
 
 #region DefaultArgs
-def add(a: int, b: int = 0, c: int = 0)->int:
-    print(f"{a = }, {b = }, {c = }")
-    sum  = a + b + c
+# def add(a: int, b: int = 0, c: int = 0)->int:
+#     print(f"{a = }, {b = }, {c = }")
+#     sum  = a + b + c
+#     return sum
+
+# print(add(1, 2))
+# print(add(1, 2, 3))
+#endregion
+
+
+#region PositionalArgs
+# def add(a, b, c)->int:
+#     print(f"{a = }, {b = }, {c = }")
+#     sum  = a + b + c
+#     return sum
+
+# print(add(1, 2, 3))
+# print(add(3, 2, 1))
+#endregion
+
+
+#region NamedArgs/KeywordedArgs
+# def add(a, b, c=0)->int:
+#     print(f"{a = }, {b = }, {c = }")
+#     sum  = a + b + c
+#     return sum
+
+# print(add(a=1, b=2, c=3))
+# print(add(c=3, b=2, a=1))
+
+## open("Test.txt", closefd=False, encoding="UTC-16")
+#endregion
+
+
+#region (un)packing
+# def add(a, b, c=0)->int:
+#     print(f"{a = }, {b = }, {c = }")
+#     sum  = a + b + c
+#     return sum
+
+# # a = 10
+# # b = 20
+# # c = 30
+# a, b, c = 10, 20, 30
+# print(add(a, b, c))
+
+# data = [100, 200, 300]
+# print(add(data[0], data[1], data[2]))
+
+# data = [100, 200, 300]
+# # p, q, r = data
+# # print(add(p, q, r))
+# print(add(*data))     # <-- Unpacks in the call (point where the data starts the journey; before actual call operation)
+
+
+# ## Packing
+
+# data = [1, 2, 3, 4, 5]
+# # a, b, *rest = data
+# # print(f"{a = }, {b = }, {rest = }")
+
+# a, b, *_ = data
+# print(f"{a = }, {b = }")
+
+# a, *_, b = data
+# print(f"{a = }, {b = }")
+
+# a, *_, b, _ = data
+# print(f"{a = }, {b = }")
+
+# a, _, b, *_ = data
+# print(f"{a = }, {b = }")
+
+#endregion
+
+
+#region VariableArgs
+def add(a, b, *others)->int:
+    print(f"{type(a) = }, {type(b) = }, {type(others) = }")
+    print(f"{a = }, {b = }, {others = }")
+    sum  = a + b
+    for val in others:
+        sum += val
     return sum
 
 print(add(1, 2))
 print(add(1, 2, 3))
+print(add(1, 2, 3, 4))
+print(add(1, 2, 3, 4, 5))
 #endregion
 
